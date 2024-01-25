a67cedfbe3039dad89210b8e94f262826d51c8b3
李建伟 Jian Wei Li <jarvis.li@adprod.derbysoft.com>
2024-01-25 12:44:53 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import CompressionPlugin from 'compression-webpack-plugin';
 import { terser } from 'rollup-plugin-terser';
 
 export default defineConfig({
-  base: './Vindicate',
+  base: '/Vindicate/',
   plugins: [
     vue(),
   ],
